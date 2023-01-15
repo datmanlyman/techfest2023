6c6266386d470a59169816a8d4e1d047be41cc6b
Wei Xing <weixingp5@gmail.com>
2023-01-15 22:21:48 +0800

    add direct statics

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, send_from_directory
 
 app = Flask(__name__)
 
@@ -12,3 +12,7 @@ def home():
 @app.route("/quiz")
 def quiz():
     return render_template('quiz.html')
+
+@app.route("/<path:path>")
+def render_service_worker(path):
+    return send_from_directory('./direct_static', path)
