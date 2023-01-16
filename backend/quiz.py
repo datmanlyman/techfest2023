7d5d541be87754c6d97268be1c64d72ce394c924
datmanlyman <jeewern2@gmail.com>
2023-01-16 11:50:46 +0800

    added the structures for the urls

--- backend/quiz.py
+++ backend/quiz.py
@@ -45,6 +45,12 @@ class DoublyLinkedList:
 
         return node
 
+    def view(self):
+        node = self.head
+        while node is not None:
+            print(node.data)
+            node = node.next
+
 
 def LaplaceSmoothing(numberOfEach):  # Should be a single integer per row
     totalNumber = sum(numberOfEach)  # For the total number
@@ -56,25 +62,56 @@ def LaplaceSmoothing(numberOfEach):  # Should be a single integer per row
     return numberOfEach  # Should become floats
 
 
-def PlotLine(numberOfEach):
+def PlotLine(probabilityOfEach):
     line = []
     total = 0
 
-    for number in numberOfEach:
+    for probability in probabilityOfEach:
         # range for each job class is inclusive of total + number
-        line.append(total + number)
-        total += number
+        line.append(total + probability)
+        total += probability
 
     return line
 
 
 def GetJobType(line):
     randomNumber = random.random()
-    jobClass = float('inf')
     for idx in range(len(line)):
         endNumber = line[idx]
         if randomNumber <= endNumber:
-            jobClass = idx
-            break
+            return idx
+    return 0
 
-    return jobClass
+
+def GetJob(jobQueues, line):
+    seen = [0 for _ in range(5)]
+    jobClass = GetJobType(line)
+    seen[jobClass] = 1
+    jobQueue = jobQueues[jobClass]
+    while not jobQueue:
+        jobClass = GetJobType(line)
+        if seen[jobClass] == 1:
+            print(f"{jobClass} has already been accessed")
+            continue
+        seen[jobClass] = 1
+        jobQueue = jobQueues[jobClass]
+        if not jobQueue and sum(seen) == 5:
+            print("All empty!")
+            return
+    return jobQueue.popleft()
+
+
+def SetJob(viewQueue):
+    node = viewQueue.popHead()
+    return node.data
+
+
+numberOfEach = [3, 3, 1, 1, 2]
+probabilityOfEach = LaplaceSmoothing(numberOfEach)
+line = PlotLine(probabilityOfEach)
+jobQueues = [deque([i]) for i in range(5)]
+viewQueue = DoublyLinkedList()
+for _ in range(6):
+    viewQueue.insertTail(GetJob(jobQueues, line))
+
+viewQueue.view()
