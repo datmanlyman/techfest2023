b36e58aff912df05679ed74307814c64a3cffadc
datmanlyman <jeewern2@gmail.com>
2023-01-16 23:20:40 +0800

    completed the showing onto frontend

--- static/scripts/quiz.js
+++ static/scripts/quiz.js
@@ -2,20 +2,32 @@ const totalJobClass = 5
 let quizResults = [];
 let ranking = [];
 
-let object = {
-    'Software Engineering': {
-        picture: './static/images/careers/innovation.jpg',
-        title: 'Software Engineering',
-        example: 'Frontend Engineer, Backend Engineer, Full Stack Developer'
-    }, 'AI': {
-        picture: './static/images/careers/innovation.jpg',
-        title: 'Artificial Intelligence',
-        example: 'Frontend Engineer, Backend Engineer, Full Stack Developer'
-    }
-}
-
 for (let i = 0; i < totalJobClass; i++) {
     quizResults.push(0);
+}
+
+let object = {
+    'Software Engineering': {
+        picture: 'static/images/careers/software-engineer.jpg',
+        title: 'Software Engineering',
+        example: 'Frontend Engineer, Backend Engineer, Full Stack Developer'
+    }, 'Cyber Security': {
+        picture: 'static/images/careers/cyber-security.jpg',
+        title: 'Cyber Security',
+        example: 'Malware Analyst, Penetration Tester, Security Architect'
+    }, 'Hardware': {
+        picture: 'static/images/careers/innovation.jpg',
+        title: 'Hardware',
+        example: 'IoT Developer, Embedded Systems, Signal Processing'
+    }, 'AI': {
+        picture: 'static/images/careers/innovation.jpg',
+        title: 'Artificial Intelligence',
+        example: 'NLP Engineer, AI Engineer, Data Scientist'
+    }, 'System Analyst': {
+        picture: 'static/images/careers/innovation.jpg',
+        title: 'System Analyst',
+        example: 'Software Analyst, Quality Engineer, System Analyst'
+    }
 }
 
 function getAnswer() {
@@ -49,7 +61,7 @@ function heapSort() {
         ranking.push(jobClass);
     }
 
-    console.log(ranking);
+    showRanking();
 }
 
 function createMap(array) {
@@ -75,7 +87,7 @@ function referTable(idx) {
         case 3:
             return "AI";
         case 4:
-            return "Software Analysis";
+            return "System Analyst";
     }
 }
 
@@ -106,4 +118,14 @@ function swap(i, j, array) {
     const temp = array[j];
     array[j] = array[i];
     array[i] = temp;
+}
+
+function showRanking() {
+    for (let idx = 0; idx < totalJobClass; idx++) {
+        const job = ranking[idx];
+        picture, title, example = object[job];
+        document.getElementById("picture" + idx + 1).src = picture;
+        document.getElementById("title" + idx + 1).innerHTML = title;
+        document.getElementById("example" + idx + 1).innerHTML = example;
+    }
 }
