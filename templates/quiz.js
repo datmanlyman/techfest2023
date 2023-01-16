b00b21ebb148db028f353492269e482caf85252d
datmanlyman <jeewern2@gmail.com>
2023-01-16 15:56:29 +0800

    added the answers checking

---
+++ templates/quiz.js
@@ -0,0 +1,26 @@
+var quizResults = [];
+
+function getAnswer() {
+    var radioID = document.querySelector('input[name="inlineRadioOptions"]:checked').id;
+    var result = radioID.substring(radioID.length - 1);
+
+    switch (result) {
+        case "1":
+            quizResults.push("A");
+            break;
+        case "2":
+            quizResults.push("B");
+            break;
+        case "3":
+            quizResults.push("C");
+            break;
+        case "4":
+            quizResults.push("D");
+            break;
+        case "5":
+            quizResults.push("E");
+            break;
+    }
+
+    console.log(quizResults)
+}
